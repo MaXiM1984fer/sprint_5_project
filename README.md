facdbbb3e38d3e1243105518c2054f1677cf7c1b
MaXiM1984fer <privateojeda@gmail.com>
2024-02-21 01:36:25 -0500

    Segunda version de streamlit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # sprint_5_project
+
+URL de render del proyecto: https://sprint-5-project-71f1.onrender.com/
--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
         
+st.header('Anuncios de Autos Usados US')
+
+st.write('Análisis exploratorio del mercado de autos usados de los Estados Unidos de America')
+
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
         
@@ -13,4 +17,18 @@ if hist_button: # al hacer clic en el botón
     fig = px.histogram(car_data, x="odometer")
         
     # mostrar un gráfico Plotly interactivo
-    st.plotly_chart(fig, use_container_width=True) 
+    st.plotly_chart(fig, use_container_width=True)
+
+
+
+scatter_button = st.button('Construir gráfico de dispersión') # crear un botón
+        
+if scatter_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+            
+    # crear un gráfico de dispersión
+    fig = px.scatter(car_data, x="model_year")
+        
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
