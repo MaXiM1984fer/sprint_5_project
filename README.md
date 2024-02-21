2b4bc4ba37de6fc2d693a7f3961682897da9014e
MaXiM1984fer <privateojeda@gmail.com>
2024-02-21 01:51:49 -0500

    V2.5 de streamlit

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # sprint_5_project
 
+EL proyecto se basa en mostrar las funcionalidades de Streamlit en combinacion con Render desde un repotitorio en Github.
+El analisis exploratorio de la data presentada y los graficos mostrados es acerca de anuncios de ventas de automóviles usados en USA.
+
 URL de render del proyecto: https://sprint-5-project-71f1.onrender.com/
--- app.py
+++ app.py
@@ -7,9 +7,9 @@ st.header('Anuncios de Autos Usados US')
 st.write('Análisis exploratorio del mercado de autos usados de los Estados Unidos de America')
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
+hist_check = st.checkbox('Construir histograma') # crear un botón
         
-if hist_button: # al hacer clic en el botón
+if hist_check: # al hacer clic en el botón
     # escribir un mensaje
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
             
@@ -21,14 +21,14 @@ if hist_button: # al hacer clic en el botón
 
 
 
-scatter_button = st.button('Construir gráfico de dispersión') # crear un botón
+scatter_check = st.checkbox('Construir gráfico de dispersión') # crear un botón
         
-if scatter_button: # al hacer clic en el botón
+if scatter_check: # al hacer clic en el botón
     # escribir un mensaje
-    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    st.write('Creación de un gráfico de dispersión del precio de los anuncios de venta de coches')
             
     # crear un gráfico de dispersión
-    fig = px.scatter(car_data, x="model_year")
+    fig = px.scatter(car_data, x="price")
         
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
